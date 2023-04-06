d62c4eab8e9979b56bc1ae5a89bdf1aaad26bb17
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-04-06 09:15:50 -0600

    a

--- ejercicioTarea7.cpp
+++ ejercicioTarea7.cpp
@@ -1,8 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <iostream>
 
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    cout<<(stderr, "Usage: %s <num_children>\n", argv[0]);
+    exit(1);
+  }
 
-    return 0;
+  int num_children = atoi(argv[1]);
+  if (num_children <= 0) {
+    cout<<(stderr, "Error: Invalid number of children\n");
+    exit(1);
+  }
+
+  cout<<("Parent process: PID=%d, PPID=%d\n", getpid(), getppid());
+
+  // Declarar una variable para contar los hijos creados
+  int count = 1;
+
+  // Usar un bucle while en vez de un for
+  while (count <= num_children) {
+      pid_t pid = fork();
+      if (pid < 0) {
+          cout<<(stderr, "Fork failed\n");
+          exit(1);
+      } else if (pid == 0) {
+          cout<<("Child #%d: PID=%d, PPID=%d\n", count, getpid(), getppid());
+          exit(0);
+      } else {
+          wait(NULL);
+      }
+
+      // Incrementar la variable en cada iteración
+      count++;
+  }
+
+  return 0;
 }
+
